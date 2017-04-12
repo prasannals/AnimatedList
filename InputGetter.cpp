f6aa33414684b157a4b3580c101e22700a019878
prasannals <prasanna.pessu@gmail.com>
2017-04-12 15:07:55 +0530

    Input Parser completed

--- InputGetter.cpp
+++ InputGetter.cpp
@@ -8,11 +8,19 @@ std::string AnimationTask::getExtra(std::string key){
 	return this->extras[key];
 }
 
-AnimationTask::AnimationTask(int id){
+AnimationTask::AnimationTask(){
+
+}
+
+void AnimationTask::setId(int id){
 	this->id = id;
 }
 
-void AnimationTask::addExtra(std::string key, std::string value){
+AnimationTask::AnimationTask(int id){
+	this->setId(id);
+}
+
+void AnimationTask::putExtra(std::string key, std::string value){
 	this->extras[key] = value;
 }
 
@@ -45,7 +53,7 @@ char* FileParser::readFromFile(const char *filename){
 	return buf;
 }
 
-void InputParser::split(std::string str, char *delimiter, std::vector<std::string> &vec){
+void InputParser::split(std::string str, const char *delimiter, std::vector<std::string> &vec){
 	char *token = strtok((char*)str.c_str(), delimiter);
 	
 	while(token != NULL){
@@ -58,9 +66,106 @@ void InputParser::split(std::string str, char *delimiter, std::vector<std::strin
 std::string FileParser::getInput(){
 	std::string allCommands(this->readFromFile(this->filename.c_str()));
 	
+	return allCommands;
 }
 
+std::vector<AnimationTask> InputParser::getTasks(){
+	std::string allCommands = this->getInput();
+	std::vector<AnimationTask> tasks;
+
+
+	std::vector<std::string> lines;
+	this->split(allCommands, "\n", lines);
+
+	for(int i = 0; i < lines.size(); i++){
+		std::string line = lines.at(i);
+
+		std::vector<std::string> tokens;
+		this->split(line, " ", tokens);
+
+		AnimationTask task;
+		
+		
+		///////////// NOTE : MORE VALIDATION NEEDED LATER ///////////////////
+		switch(tokens.size()){
+		case 2:
+			if(strcmp(tokens.at(0).c_str(), "add") == 0 ){
+				//std::cout << "add\n";
+				task.setId(ADD_ID);
+				task.putExtra(ELEMENT_EXTRA, tokens.at(1));
+			}else if(strcmp(tokens.at(0).c_str(), "delete") == 0){
+				//std::cout << "delete\n";
+				task.setId(DELETE_ID);
+				task.putExtra(INDEX_EXTRA, tokens.at(1));
+			}else if(strcmp(tokens.at(0).c_str(), "set") == 0){
+				//std::cout << "set\n";
+				task.setId(SET_ID);
+				std::vector<std::string> sp;
+				this->split(tokens.at(1), "," , sp);
+				
+				task.putExtra(INDEX_EXTRA, sp.at(0));
+				task.putExtra(ELEMENT_EXTRA, sp.at(1));
+			}else if(strcmp(tokens.at(0).c_str(), "highlight") == 0){
+				//std::cout << "highlight\n";
+				task.setId(HIGHLIGHT_ID);
+				
+				task.putExtra(INDEX_EXTRA, tokens.at(1));
+			}else if(strcmp(tokens.at(0).c_str(), "swap") == 0){
+				//std::cout << "swap\n";
+				task.setId(SWAP_ID);
+				std::vector<std::string> sp;
+				this->split(tokens.at(1), "," , sp);
+				
+				task.putExtra(INDEX_ONE_EXTRA, sp.at(0));
+				task.putExtra(INDEX_TWO_EXTRA, sp.at(1));
+			}else{
+				//std::cout << "something else\n";
+			}
+			
+			tasks.push_back(task);
+			
+			break;
+		default:
+			break;	
+		}
+	}
+	
+	return tasks;
+}
 
 FileParser::FileParser(std::string filename){
 	this->filename = filename;
+}
+
+
+int main(int argc, char **argv){
+	FileParser fp(argv[1]);
+	
+	std::vector<AnimationTask> tasks = fp.getTasks();
+	
+	for(int i =0 ; i < tasks.size(); i++){
+		AnimationTask current = tasks.at(i);
+		switch(current.getId()){
+			case ADD_ID:
+				
+				std::cout << "add : " << current.getExtra(ELEMENT_EXTRA) << std::endl;
+				break;
+			case DELETE_ID:
+				std::cout << "delete : " << current.getExtra(INDEX_EXTRA) << std::endl;
+				break;
+			case SET_ID:
+				std::cout << "set : " << current.getExtra(INDEX_EXTRA) << " :: " << current.getExtra(ELEMENT_EXTRA) << std::endl;
+				break;
+			case SWAP_ID:
+				std::cout << "swap : " << current.getExtra(INDEX_ONE_EXTRA) << " :: " << current.getExtra(INDEX_TWO_EXTRA) << std::endl;
+				break;
+			case HIGHLIGHT_ID:
+				std::cout << "highlight : " << current.getExtra(INDEX_EXTRA) << std::endl;
+				break;
+			default:
+				std::cout << "new encounter...." << std::endl;
+		}
+	}
+	
+return 0;
 }
--- InputGetter.h
+++ InputGetter.h
@@ -1,29 +1,44 @@
 #ifndef INPUT_GETTER_H
 #define INPUT_GETTER_H
 
+
+#define ADD_ID 0
+#define DELETE_ID 1
+#define SET_ID 3
+#define HIGHLIGHT_ID 4
+#define SWAP_ID 5
+#define ELEMENT_EXTRA "element"
+#define INDEX_EXTRA "index"
+#define INDEX_ONE_EXTRA "index1"
+#define INDEX_TWO_EXTRA "index2"
+
 #include<map>
 #include<iostream>
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#include<vector>
 
 class AnimationTask{
 private:
 	int id;
 	std::map<std::string, std::string> extras;
 public:
+	AnimationTask();
 	AnimationTask(int id);
 	int getId();
+	void setId(int id);
 	std::string getExtra(std::string key);
-	void addExtra(std::string key, std::string value);
+	void putExtra(std::string key, std::string value);
 };
 
 
 class InputParser{
 protected:
-	void split(std::string str, char *delimiter, std::vector<std::string> &vec);
+	void split(std::string str, const char *delimiter, std::vector<std::string> &vec);
 public:
 	virtual std::string getInput() = 0;
+	std::vector<AnimationTask> getTasks();
 };
 
 class FileParser: public InputParser{
